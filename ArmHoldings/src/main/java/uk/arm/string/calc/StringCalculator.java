2b8b3fb922acce328e8f566d9c18629d5e05a6a7
Aamirio <aamiridrees@hotmail.com>
2015-09-01 17:03:58 +0100

    Step 1 complete. Tests passing for zero, one, or two arguments.

--- ArmHoldings/src/main/java/uk/arm/string/calc/StringCalculator.java
+++ ArmHoldings/src/main/java/uk/arm/string/calc/StringCalculator.java
@@ -1,11 +1,37 @@
 package uk.arm.string.calc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
 	
 	public int add(String numbers) {
 		int sumTotal = 0;
 		
+		for (Integer number : parseString(numbers)) {
+			sumTotal += number;
+		}
+		
 		return sumTotal;
 	}
+    
+    public List<Integer> parseString(String numbers) throws IllegalArgumentException {
+    	List<Integer> numberCollection = new ArrayList<Integer>();
+    	
+    	try {
+    		List<String> numbersAsStrings = Arrays.asList(numbers.split(","));
+    		if (numbersAsStrings.size() > 2) {
+    			throw new IllegalArgumentException();
+    		}
+	    	for (String numberAsString : numbersAsStrings) {
+	    		int number = numberAsString.trim().equals("") ? 0 : Integer.valueOf(numberAsString.trim());
+	    		numberCollection.add(number); 
+	    	}
+    	} catch (NumberFormatException nfe) {
+    		throw new IllegalArgumentException(nfe.toString());
+    	}
+    	return numberCollection;
+    }
 
 }
